2fa8fc6862c9fde11103cc8b9910b23f73b33dc3
cmr <cmr@Ubuntu.myguest.virtualbox.org>
2024-01-10 21:08:47 -0500

    Note Update

--- hello/hw.cpp
+++ hello/hw.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl; 
+    cout << "Hello World!" << endl;
+    cout << "This program is part of my studies of the c++ is for everyone book.\n"; 
+    cout << "Here I will practice some of the things the text book explains.\n";
     return 0;
 }
